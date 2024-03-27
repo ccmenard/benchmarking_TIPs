8de4e05cc8c4e03a6767f12d569f02277aa58a54
Claire Menard <42971237+ccmenard@users.noreply.github.com>
2024-03-27 13:22:41 -0500

    Create processing_TIP_finder_output.sh

---
+++ TIP_finder/processing_TIP_finder_output.sh
@@ -0,0 +1,16 @@
+#TIP_finder output proccessing
+#merge overlapping read windows into coordinates
+module load bedtools
+bedtools merge -i Ler0-vs-ALL.sort.bed > merged_windows_Ler0-vs-ALL.sort.bed 
+#sort -V -k1,1 -k2,2 Oh43-vs-ALL.sort.bed > fixed.Oh43-vs-ALL.sort.bed
+#bedtools merge -i fixed.Oh43-vs-ALL.sort.bed > merged_windows_Oh43-vs-ALL.sort.bed
+
+mkdir results/
+cd results/
+cp ../merged_windows_Ler0-vs-ALL.sort.bed .
+ln -s /home/hirschc1/menar060/programs/input_data/Arabidopsis/final/TAIR10_GFF3_genes.gff.bed.genes.bed
+ln -s /home/hirschc1/menar060/programs/input_data/Arabidopsis/final/filtered_TAIR10_GCF_000001735.4.fasta.mod.EDTA.TEanno.gff3
+awk 'BEGIN{ FS = OFS = "\t" } { print $0,"TIP_finder_"NR"_CM"}' merged_windows_Ler0-vs-ALL.sort.bed > named.merged_windows_Ler0-vs-ALL.sort.bed
+#ln -s /home/hirschc1/menar060/programs/input_data/maize/B73v5_genes.bed
+#ln -s /home/hirschc1/menar060/programs/input_data/maize/filtered_B73.EDTA.TEanno.gff3
+awk 'BEGIN{ FS = OFS = "\t" } { print $0,"TIP_finder_"NR"_CM"}' merged_windows_Oh43-vs-ALL.sort.bed > named.merged_windows_Oh43-vs-ALL.sort.bed
